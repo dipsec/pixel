b2c2e9b015867d253be1c6bc7fdd51e1bc12bb20
liorvh <liorvh@gmail.com>
2015-07-26 05:36:33 +0300

    Update pixel.sh

--- pixel.sh
+++ pixel.sh
@@ -5,6 +5,7 @@ wget https://raw.githubusercontent.com/liorvh/pixel/master/track.php
 touch 1.txt
 chmod a+w 1.txt
 
+clear
 echo "Email to send to:"
 read email
 echo
@@ -14,4 +15,3 @@ echo "Your external ip is: $myIP"
 echo
 echo "Your tacking URL is:"
 echo "http://$myIP/track.php?e=$email"
-clear
