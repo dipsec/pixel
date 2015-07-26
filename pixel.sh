8b716bab319cf059234f189fac32c16215f0de3d
liorvh <liorvh@gmail.com>
2015-07-26 05:28:13 +0300

    Create pixel.sh

---
+++ pixel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /var/www
+wget https://raw.githubusercontent.com/liorvh/pixel/master/track.php
+touch 1.txt
+chmod a+w 1.txt
+
+echo "Email to send to:"
+read email
+echo
+
+myIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+echo "Your external ip is: $myIP"
+echo
+echo "Your tacking URL is:"
+echo "http://$myIP/track.php?e=$email"
